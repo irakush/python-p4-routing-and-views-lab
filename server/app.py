07c1de2fae7b9f964791f39392d0284b9908ca1a
Igor Rakush <irakush@aol.com>
2024-01-29 15:20:17 -0500

    done

--- server/app.py
+++ server/app.py
@@ -4,5 +4,43 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
+
+
+@app.route("/")
+def index():
+    return ("<h1>Python Operations with Flask Routing and Views</h1>", 200)
+
+
+@app.route("/print/<string:parameter>")
+def print_string(parameter):
+    print(parameter)
+    return (parameter, 200)
+
+
+@app.route("/count/<int:parameter>")
+def count(parameter):
+    res = ""
+    for i in range(parameter):
+        res += str(i) + "\n"
+
+    return (res, 200)
+
+
+@app.route("/math/<int:num1>/<string:operation>/<int:num2>")
+def math(num1, operation, num2):
+    print(num1, operation, num2)
+
+    if operation == "+":
+        return (str(num1 + num2), 200)
+    elif operation == "-":
+        return (str(num1 - num2), 200)
+    elif operation == "*":
+        return (str(num1 * num2), 200)
+    elif operation == "div":
+        return (str(float(num1 / num2)), 200)
+    elif operation == "%":
+        return (str(num1 % num2), 200)
+    else:
+        return ("wrong operation", 200)
